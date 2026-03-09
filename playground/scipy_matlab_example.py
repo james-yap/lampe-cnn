8da8b390ab08356ef17bbc0256ddfe5e5a347d22
James Yap <contact@jamesyap.org>
2026-03-09 15:33:18 -0400

    feat(examples): 3-axes setup

--- playground/scipy_matlab_example.py
+++ playground/scipy_matlab_example.py
@@ -3,14 +3,27 @@ import matplotlib.pyplot as plt
 
 # Import MATLAB data using scipy.io
 raw_data = sio.loadmat('playground_dataset/Healthy_bulk_data.mat')
+
+# Keys available for use
+print(raw_data.keys())
+
+# Shape of image matrix
+srs_1450_images = raw_data['Healthy_1450_bgsub']
+srs_1668_images = raw_data['Healthy_1668_bgsub']
 shg_images = raw_data['Healthy_SHG']
 print(shg_images.shape)
 
-# Plot an image as an example
-plt.figure(figsize=(6, 6))
-plt.imshow(shg_images[:,:,10], cmap='viridis')
-plt.colorbar()
-plt.title("Healthy (SHG)")
-# plt.xlabel("X-axis")
-# plt.ylabel("Y-axis")
+# Using matplotlib
+fig, axes = plt.subplots(1, 3, figsize=(15,5))
+
+srs_1450 = axes[0].imshow(srs_1450_images[:,:,5], cmap='jet')
+axes[0].set_title('SRS 1450 cm$^{-1}$')
+
+srs_1668 = axes[1].imshow(srs_1668_images[:,:,5], cmap='jet')
+axes[1].set_title('SRS 1668 cm$^{-1}$')
+
+shg = axes[2].imshow(shg_images[:,:,5], cmap='jet')
+axes[2].set_title('SHG')
+
+plt.tight_layout()
 plt.show()
