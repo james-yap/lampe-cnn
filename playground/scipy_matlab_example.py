afcae546de73d2e5ae63b18e0d53a43179783b52
James Yap <contact@jamesyap.org>
2026-03-09 15:26:00 -0400

    feat(example): added plot

--- playground/scipy_matlab_example.py
+++ playground/scipy_matlab_example.py
@@ -1,4 +1,16 @@
 import scipy.io as sio
+import matplotlib.pyplot as plt
 
-data = sio.loadmat('playground_dataset/Healthy_bulk_data.mat')
-print(data['Healthy_SHG'].shape)
+# Import MATLAB data using scipy.io
+raw_data = sio.loadmat('playground_dataset/Healthy_bulk_data.mat')
+shg_images = raw_data['Healthy_SHG']
+print(shg_images.shape)
+
+# Plot an image as an example
+plt.figure(figsize=(6, 6))
+plt.imshow(shg_images[:,:,10], cmap='viridis')
+plt.colorbar()
+plt.title("Healthy (SHG)")
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+plt.show()
