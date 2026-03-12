5204eabd584971a86aa5d9c9f95a7a89f03c3c43
Vipra Hareshbhai Gajera <Vipra22ca@gmail.com>
2026-03-12 19:10:32 -0400

    Base ResNet-18 Model
    Just working model. in process for fine tuning.

---
+++ base-ResNet/model.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import random_split
+
+def ResNet18(num_classes=4):
+    # loading a pretrained resnet-18 model
+    model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+
+    # changing the final layer
+    model.fc = nn.Linear(in_features=512, out_features=num_classes)
+
+    # freezing every layer, except the last 2 blocks
+    for name, param in model.named_parameters():
+        if "layer4" in name or "fc" in name:
+            param.requires_grad = True   # fine-tune these
+        else:
+            param.requires_grad = False  # freeze others
+
+    return model
+
+
